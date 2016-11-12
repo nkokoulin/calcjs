071a8e000d671f62a80468bc8f558d41e6695354
Nikolay Kokoulin <Nikolay Kokoulin>
2016-11-12 23:48:45 +0300

    first part of advanced calculations are made

--- public/js/calc.js
+++ public/js/calc.js
@@ -1,13 +1,49 @@
+'use strict'
+/* TODO: 
+	Добавить десятичные числа
+	Сделать точные вычисления десятичных чисел
+*/
+
 function Calc(exression) {
+	//expression используется для отображения формулы на дисплее калькулятора
 	this.expression = exression || '';
+	//buffer используется для записи чисел
+	this.buffer = '';
+	this.signCount = 0;
+	this.digitCount = 0;
+	this.dotCount = 0;
+	this.thereIsDot = 0;
+	this.result = 0;
+
+	//Вычисление всегда происходит по 1 математической операции за один раз
+	this.firstNumber = undefined;
+	this.firstNumberDots = 0;
+	this.secondNumber = undefined;
+	this.secondNumberDots = 0;
+	this.currentSign = undefined;
 };
 
-//Выполнение записанного выражения
+Calc.prototype.evalExpression = function(expression) {
+
+}
+// Calc.eval() возвращает результат, сбрасывая текущие цифры и знак, и ничего не печатает
 Calc.prototype.eval = function() {
-	let result = eval(this.expression);
-	this.writeResultToDisplay(result);
-	this.writeExpressionToHistory(this.expression + '=' + result);
-	this.expression = result;
+	this.addNumberFromBuffer();
+	this.clearBuffer();
+	if (this.secondNumber !== undefined) {
+		var toPerform = this.firstNumber + this.currentSign + this.secondNumber;
+		var result = eval(toPerform);
+
+		this.writeExpressionToHistory(toPerform + '=' + result);
+	
+		this.firstNumber = result;
+		this.secondNumber = undefined;
+		this.currentSign = undefined;
+
+		this.expression = result;
+		this.writeExpressionToDisplay();
+		this.result = result;
+	} 
 }
 
 Calc.prototype.clearDisplay = function() {
@@ -25,16 +61,58 @@ Calc.prototype.writeExpressionToDisplay = function() {
 	inputDisplay.value = this.expression;
 }
 
+Calc.prototype.clearBuffer = function() {
+	this.buffer = '';
+}
+
 Calc.prototype.writeResultToDisplay = function(result) {
 	inputDisplay.value = result;
 }
 
-Calc.prototype.addSign = function(sign) {
-	this.expression += sign;
+Calc.prototype.addDot = function() {
+	this.thereIsDot = true;
+	this.expression += '.';
 	this.writeExpressionToDisplay();
 }
 
+Calc.prototype.addSign = function(sign) {
+	if (this.firstNumber === undefined && this.buffer === '') {
+		//вернуть ошибку
+	} else if (this.firstNumber === undefined && this.digitCount > 0) {
+		this.addNumberFromBuffer();
+		this.clearBuffer();
+	} else if (this.firstNumber !== undefined && this.digitCount > 0) {
+		this.addNumberFromBuffer();
+		this.clearBuffer();
+		this.eval();
+		this.firstNumber = this.result;
+	}
+	this.currentSign = sign;
+	this.expression = this.firstNumber + this.currentSign;
+	this.writeExpressionToDisplay();
+}
+
+Calc.prototype.addNumberFromBuffer = function() {
+	if (this.buffer !== '') {
+		if (this.firstNumber === undefined) {
+			this.firstNumber = +this.buffer;
+			this.firstNumberDots = this.dotCount;
+		} else if (this.secondNumber === undefined) {
+			this.secondNumber = +this.buffer;
+			this.secondNumberDots = this.dotCount;
+		} else {
+			//Выдать ошибку, о том, что невозможно добавить третье число
+		}
+		this.digitCount = 0;
+	}
+}
+
 Calc.prototype.addDigit = function(digit) {
+	this.digitCount++;
+	if (this.thereIsDot) {
+		this.dotCount++;
+	}
+	this.buffer += digit.toString();
 	this.expression += digit.toString();
 	this.writeExpressionToDisplay();
 }
